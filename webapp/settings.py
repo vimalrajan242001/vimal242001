f9bc16bd7829e88127f229a1bc661bcd1a422e33
vimalrajan242001 <64751374+vimalrajan242001@users.noreply.github.com>
2020-05-05 23:50:51 +0530

    Update settings.py

--- webapp/settings.py
+++ webapp/settings.py
@@ -113,8 +113,11 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+import dj_database_url
 
 
+db_from_env=dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
